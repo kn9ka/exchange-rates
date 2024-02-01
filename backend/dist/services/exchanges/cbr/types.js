657b86b4b4c0ef69db5aa3f97bf161b8b452ddfe
Anton Krackov <kn9kaa+github@gmail.com>
2024-02-01 04:53:22 +0400

    feat(backend): fix action

--- backend/dist/services/exchanges/cbr/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Currency = void 0;
-exports.Currency = {
-    RUB: "RUB",
-    USD: "USD",
-    GEL: "GEL",
-    EUR: "EUR",
-};
--- backend/dist/services/exchanges/corona/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Currency = void 0;
-exports.Currency = {
-    RUB: "810",
-    USD: "840",
-    GEL: "981",
-    EUR: "978",
-};
